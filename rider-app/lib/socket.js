d34bdb117293727f82b5ae6f024acd49623f828e
@Phantom101 <valdoramakuela@gmail.com>
2025-08-26 12:22:08 -0400

    Update socket.js

--- rider-app/lib/socket.js
+++ rider-app/lib/socket.js
@@ -1,4 +1,18 @@
-// Use Render URL
-const socket = io('https://ridehail-backend.onrender.com', {
-  transports: ['websocket'],
-});  
+import io from 'socket.io-client';
+
+const SOCKET_URL = 'https://ridehail-backend.onrender.com';
+
+const socket = io(SOCKET_URL, {
+  transports: ['websocket', 'polling'],
+  timeout: 20000,
+});
+
+socket.on('connect', () => {
+  console.log('Connected to server');
+});
+
+socket.on('disconnect', () => {
+  console.log('Disconnected from server');
+});
+
+export default socket;
